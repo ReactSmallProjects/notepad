4a8b377388ea69b9c96e79ac3520c0f25b42fc77
Monikaverma2001 <monika8427084@gmail.com>
2023-10-12 15:09:06 +0530

    basic structure

---
+++ notepad/src/Navbar.js
@@ -0,0 +1,18 @@
+import './Navbar.css'
+function Navbar()
+{
+    return (
+        <div className="bigdiv">
+        <p className="head">Online Notepad</p>
+        <ul className="Navbar">
+    <li>File</li>
+    <li>Edit</li>
+    <li>Insert</li>
+    <li>View</li>
+    <li>Help</li>
+</ul>
+</div>
+    )
+}
+
+export default Navbar;
---
+++ notepad/src/text.js
@@ -0,0 +1,12 @@
+
+import './text.css'
+function Text()
+{
+    return (
+        <div>
+            <div></div>
+            <textarea   type="text" name="notes" />
+        </div>
+    )
+}
+export default Text;
